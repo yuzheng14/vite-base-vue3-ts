4566b9984d212efe45520d1718d5fd103e55822a
yuzheng14 <422450117@qq.com>
2022-11-24 20:05:31 +0800

    fix(webVitals.tx): no implicit any for onPerfEntry

--- webVitals.ts
+++ webVitals.ts
@@ -2,7 +2,7 @@
  * 传入参数（函数），将报告的结果传入该参数执行
  * @param {function} onPerfEntry 用于报告性能测试的函数
  */
-const reportWebVitals = (onPerfEntry) => {
+const reportWebVitals = (onPerfEntry: () => void) => {
   // 如果参数存在并且是函数
   if (onPerfEntry && onPerfEntry instanceof Function) {
     // 懒加载并获取到五种性能指标，调用报告性能测试结果的函数
