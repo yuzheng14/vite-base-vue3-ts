584c2d32bd4e24dbf707b31a13d9e908cbee195e
yuzheng14 <422450117@qq.com>
2022-12-29 12:37:39 +0800

    fix(vite.config.ts): only apply drop in prod env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   esbuild: {
-    drop: ['console', 'debugger'],
+    drop: process.env.PROD ? ['console', 'debugger'] : [],
   },
 })
